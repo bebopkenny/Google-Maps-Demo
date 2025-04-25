281fc5af7da58924a112309616e6ec02e130a3d8
kenny <kennygarcia15@csu.fullerton.edu>
2025-04-24 23:53:30 -0700

    Trying to render mapid 3:19

--- demo_route/src/app/map/page.tsx
+++ demo_route/src/app/map/page.tsx
@@ -9,15 +9,23 @@ import { APIProvider,
 import { NextApiRequest, NextApiResponse } from 'next'
 import './page.css'
 
-const GOOGLE_MAPS_API_KEY = process.env.NEXT_GOOGLE_MAP_API_KEY
+const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY;
+const GOOGLE_MAP_ID = process.env.NEXT_PUBLIC_GOOGLE_MAP_ID;
 
 const MapPage = () => {
     const position = { lat: 53.54, lng: 10 };
+    console.log('API KEY: ', GOOGLE_MAPS_API_KEY)
+    console.log('MAP ID', GOOGLE_MAP_ID)
 
     return (
         <APIProvider apiKey={GOOGLE_MAPS_API_KEY}>
-            <div >
-                <Map zoom={9} center={position}></Map>
+            <div className="google-map">
+                <Map 
+                    zoom={9} 
+                    center={position} 
+                    mapId={GOOGLE_MAP_ID}
+                >   
+                </Map>
             </div>
         </APIProvider>
     )
